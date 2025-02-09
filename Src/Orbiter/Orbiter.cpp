ed34ba56d8f2d48de3fbfc540d3d8e9b12368436
Gondos <thegondos@protonmail.com>
2025-02-09 23:32:44 +0100

    [ImGui]Convert DlgHelp

--- Src/Orbiter/Orbiter.cpp
+++ Src/Orbiter/Orbiter.cpp
@@ -664,7 +664,7 @@ VOID Orbiter::Launch (const char *scenario)
 		LOGOUT_ERR ("Scenario not found: %s", scenario);
 		TerminateOnError();
 	}
-	DlgHelp::SetScenarioHelp (pState->ScnHelp());
+	//DlgHelp::SetScenarioHelp (pState->ScnHelp());
 
 	long m0 = memstat->HeapUsage();
 	CreateRenderWindow (pConfig, scenario);
@@ -1212,7 +1212,7 @@ Vessel *Orbiter::SetFocusObject (Vessel *vessel, bool setview)
 		it->pModule->clbkFocusChanged(g_focusobj, g_pfocusobj);
 
 	if (pDlgMgr) pDlgMgr->BroadcastMessage (MSG_FOCUSVESSEL, vessel);
-	DlgHelp::SetVesselHelp (g_focusobj->HelpContext());
+	//DlgHelp::SetVesselHelp (g_focusobj->HelpContext());
 
 	return g_pfocusobj;
 }
@@ -2786,14 +2786,12 @@ HWND Orbiter::OpenDialogEx (HINSTANCE hInstance, int id, DLGPROC pDlg, DWORD fla
 	return (pDlgMgr ? pDlgMgr->OpenDialogEx (hInstance, id, hRenderWnd, pDlg, flag, context) : NULL);
 }
 
-HWND Orbiter::OpenHelp (const HELPCONTEXT *hcontext)
+void Orbiter::OpenHelp (const HELPCONTEXT *hcontext)
 {
 	if (pDlgMgr) {
 		DlgHelp *pHelp = pDlgMgr->EnsureEntry<DlgHelp> ();
-		HWND hHelp = pHelp->GetHwnd();
-		PostMessage (hHelp, WM_USER+1, 0, (LPARAM)hcontext);
-		return hHelp;
-	} else return NULL;
+		pHelp->OpenHelp(hcontext);
+	}
 }
 
 void Orbiter::OpenLaunchpadHelp (HELPCONTEXT *hcontext)
